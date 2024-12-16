f2bbfa95bc1e4554faef924d45df1541ec05961c
saeyeonKim <saeyeon220@daum.net>
2024-12-16 19:37:28 +0900

    [241216_김세연] 디자인: 회원가입페이지 퍼블리싱 완료

--- src/Pages/Signup/signup.tsx
+++ src/Pages/Signup/signup.tsx
@@ -1,7 +1,40 @@
+import { ChevronLeft } from "lucide-react";
+
 export default function SignupPage() {
   return (
-    <div>
-      <div>회원가입페이지이니다.</div>
+    <div className="w-screen h-screen bg-[#343434] flex justify-center items-center relative">
+      <div className="bg-[url('/login-bg.png')] w-full h-full bg-cover"></div>
+      <div className=" absolute w-[580px] h-[700px] bg-[#fcfcfc] rounded-2xl p-[50px] flex justify-between flex-col">
+        <div className="flex  justify-center relative">
+          <p className="text-[#767676] flex absolute left-0">
+            <ChevronLeft className="text-[#767676]" /> 뒤로가기
+          </p>
+          <img src="/login-logo.png" alt="logo" className="w-[180px] h-[60px]" />
+        </div>
+        <div className="w-full h-[400px] flex flex-col justify-between">
+          <div className="flex flex-col gap-[8px]">
+            <p className="text-[24px] text-semibold text-[#222222]">이메일</p>
+            <input type="text" className="w-full h-[48px] bg-[#fcfcfc] border-[#bdbdbd] border-b-[1px] outline-none" />
+          </div>
+          <div className="flex flex-col gap-[8px]">
+            <p className="text-[24px] text-semibold text-[#222222]">이름</p>
+            <input type="text" className="w-full h-[48px] bg-[#fcfcfc] border-[#bdbdbd] border-b-[1px] outline-none" />
+          </div>
+          <div className="flex flex-col gap-[8px]">
+            <p className="text-[24px] text-semibold text-[#222222]">비밀번호</p>
+            <input type="text" className="w-full h-[48px] bg-[#fcfcfc] border-[#bdbdbd] border-b-[1px] outline-none" />
+          </div>
+          <div className="flex flex-col gap-[8px]">
+            <p className="text-[24px] text-semibold text-[#222222]">비밀번호 확인</p>
+            <input type="text" className="w-full h-[48px] bg-[#fcfcfc] border-[#bdbdbd] border-b-[1px] outline-none" />
+          </div>
+        </div>
+        <div className="w-full h-[56px]">
+          <button className="w-full h-full rounded-2xl bg-[#32cbff] text-[#fcfcfc] text-[24px] text-semibold">
+            회원가입
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
