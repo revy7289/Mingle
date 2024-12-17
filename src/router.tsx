92b977fea826cb318c0142e693915488d98a7d2c
saeyeonKim <saeyeon220@daum.net>
2024-12-17 14:00:27 +0900

    [241217_김세연] 디자인: 비밀버호찾기 페이지 퍼블리싱 완료

--- src/router.tsx
+++ src/router.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import SignupPage from "./Pages/Signup/signup";
 import MinglePage from "./Pages/Main/mingle";
 import LoginPage from "./Pages/Login/login";
+import FindPwPage from "./Pages/FindPw/findpw";
 
 export default function Router() {
   return (
@@ -10,6 +11,7 @@ export default function Router() {
         <Route path="/" element={<MinglePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
+        <Route path="/findpw" element={<FindPwPage />} />
       </Routes>
     </BrowserRouter>
   );
