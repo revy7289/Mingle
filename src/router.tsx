c97dab38a528a0d2682b233a8ab8ae780f460c67
revy7289 <95402753+revy7289@users.noreply.github.com>
2024-12-20 15:48:46 +0900

    [241220_이하성] 디자인: 커뮤니티 등록 페이지 퍼블리싱

--- src/router.tsx
+++ src/router.tsx
@@ -5,6 +5,7 @@ import LoginPage from "./Pages/Login/login";
 import FindPwPage from "./Pages/FindPw/findpw";
 import Mypage from "./Pages/mypage";
 import Header from "./Commons/Layout/header";
+import NewPage from "./Pages/Community/New/new";
 
 const HIDDEN_HEADER = ["/", "/login", "/signup", "/findpw"];
 
@@ -29,6 +30,7 @@ export default function Router() {
           <Route path="/signup" element={<SignupPage />} />
           <Route path="/findpw" element={<FindPwPage />} />
           <Route path="/mypage" element={<Mypage />} />
+          <Route path="/community/new" element={<NewPage />} />
         </Routes>
       </Layout>
     </BrowserRouter>
