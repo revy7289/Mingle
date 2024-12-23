b9edb756005ed67e4f2ef9ac9069e9900dc0c823
revy7289 <95402753+revy7289@users.noreply.github.com>
2024-12-23 15:53:53 +0900

    [241223_이하성] 디자인: 밍글페이지 퍼블리싱, 맥북화면에 컴포 4줄 나오게 디자인 일부 수정

--- src/Pages/Home/mingle.tsx
+++ src/Pages/Home/mingle.tsx
@@ -43,46 +43,68 @@ export default function MinglePage() {
     [setEdges]
   );
 
-  function onClickMUI() {
-    setNodes((prev) => [
-      ...prev, // 기존 노드들
-      {
-        id: `node-${prev.length + 1}`,
-        type: "muiTEST",
-        position: { x: -200, y: 50 },
-        data: { value: "mui 노드 test" },
-      },
-    ]);
-  }
+  // function onClickMUI() {
+  //   setNodes((prev) => [
+  //     ...prev, // 기존 노드들
+  //     {
+  //       id: `node-${prev.length + 1}`,
+  //       type: "muiTEST",
+  //       position: { x: -200, y: 50 },
+  //       data: { value: "mui 노드 test" },
+  //     },
+  //   ]);
+  // }
 
-  function onClickANTD() {
-    setNodes((prev) => [
-      ...prev, // 기존 노드들
-      {
-        id: `node-${prev.length + 1}`,
-        type: "antdTest",
-        position: { x: -200, y: 150 },
-        data: { value: "antd 노드 test" },
-      },
-    ]);
-  }
+  // function onClickANTD() {
+  //   setNodes((prev) => [
+  //     ...prev, // 기존 노드들
+  //     {
+  //       id: `node-${prev.length + 1}`,
+  //       type: "antdTest",
+  //       position: { x: -200, y: 150 },
+  //       data: { value: "antd 노드 test" },
+  //     },
+  //   ]);
+  // }
 
   return (
     <div className="w-screen h-screen bg-[#222222] flex relative">
       {/* 사이드바 */}
       <div className="w-[240px] h-full bg-[#343434] rounded-r-2xl p-[20px] flex flex-col">
-        <img src={logo} className="pt-[60px]" />
+        <img src={logo} className="mt-[40px] px-[10px]" />
 
-        <button className="w-[200px] h-[56px] bg-[#222222] mt-[40px] text-white text-[24px] tracking-wider rounded-2xl">
+        <button className="w-[200px] h-[56px] bg-[#222222] mt-[40px] text-white text-[24px] tracking-wider rounded-2xl shrink-0">
           LOGIN
         </button>
 
-        <div className="text-white">
-          <div onClick={onClickMUI}>MUI</div>
-          <div onClick={onClickANTD}>ANTD</div>
-          <div>chakra</div>
-          <div>shadcn</div>
-        </div>
+        <div className="w-full border-b-2 border-[#767676] mt-[20px]"></div>
+
+        <ul className="text-white text-[24px] tracking-wider font-semibold flex flex-col gap-[20px] mt-[40px] px-[10px]">
+          <li>Community</li>
+          <li>My Page</li>
+          <li>Gallery</li>
+        </ul>
+
+        <div className="w-full border-b-2 border-[#767676] mt-[40px]"></div>
+
+        <p className="text-white text-[24px] tracking-wider font-semibold mt-[20px] px-[10px]">
+          Component
+        </p>
+
+        <input
+          type="text"
+          placeholder="Search"
+          className="w-[200px] h-[40px] bg-[#222222] rounded-lg mt-[20px] pl-[40px] text-white bg-[url(/search.png)] bg-no-repeat bg-[center_left_10px] shrink-0"
+        />
+
+        <ul
+          className="mt-[20px] w-full h-full max-h-[520px] overflow-scroll text-white flex flex-col gap-[20px] px-[10px]"
+          style={{ scrollbarColor: "#ffffff transparent" }}
+        >
+          {new Array(30).fill("test").map((el, idx) => (
+            <li>{`${el} ${idx + 1}`}</li>
+          ))}
+        </ul>
       </div>
 
       {/* 플로우 메인 화면 */}
