cb1f33664ccb22d01282f5adebe04ebe4b966d7a
revy7289 <95402753+revy7289@users.noreply.github.com>
2024-12-23 18:17:59 +0900

    [241223_이하성] 디자인: 밍글페이지 드로워 ui메뉴(라이브러리 리스트) 구현, 사이트 검색 zindex 수정

--- src/Pages/Home/mingle.tsx
+++ src/Pages/Home/mingle.tsx
@@ -19,6 +19,7 @@ import "@xyflow/react/dist/style.css";
 import TEST_NODE from "./testNode";
 import ANTD_ALERT from "@/components/ANTD/ANTD_ALERT";
 import MUI_ALERT from "@/components/MUI/MUI_ALERT";
+import { X } from "lucide-react";
 
 const initialNodes = [
   {
@@ -34,6 +35,7 @@ const nodeTypes = { testNode: TEST_NODE, antdTest: ANTD_ALERT, muiTEST: MUI_ALER
 export default function MinglePage() {
   const [nodes, setNodes] = useState<Node[]>(initialNodes);
   const [edges, setEdges] = useState<Edge[]>([]);
+  const [isDrawerOpen, setDrawerOpen] = useState(false);
 
   const onNodesChange = useCallback(
     (changes: NodeChange[]) => setNodes((nds) => applyNodeChanges(changes, nds)),
@@ -48,29 +50,29 @@ export default function MinglePage() {
     [setEdges]
   );
 
-  // function onClickMUI() {
-  //   setNodes((prev) => [
-  //     ...prev, // 기존 노드들
-  //     {
-  //       id: `node-${prev.length + 1}`,
-  //       type: "muiTEST",
-  //       position: { x: -200, y: 50 },
-  //       data: { value: "mui 노드 test" },
-  //     },
-  //   ]);
-  // }
+  function onClickMUI() {
+    setNodes((prev) => [
+      ...prev, // 기존 노드들
+      {
+        id: `node-${prev.length + 1}`,
+        type: "muiTEST",
+        position: { x: -200, y: 50 },
+        data: { value: "mui 노드 test" },
+      },
+    ]);
+  }
 
-  // function onClickANTD() {
-  //   setNodes((prev) => [
-  //     ...prev, // 기존 노드들
-  //     {
-  //       id: `node-${prev.length + 1}`,
-  //       type: "antdTest",
-  //       position: { x: -200, y: 150 },
-  //       data: { value: "antd 노드 test" },
-  //     },
-  //   ]);
-  // }
+  function onClickANTD() {
+    setNodes((prev) => [
+      ...prev, // 기존 노드들
+      {
+        id: `node-${prev.length + 1}`,
+        type: "antdTest",
+        position: { x: -200, y: 150 },
+        data: { value: "antd 노드 test" },
+      },
+    ]);
+  }
 
   return (
     <div className="w-screen h-screen bg-[#222222] flex relative">
@@ -84,6 +86,7 @@ export default function MinglePage() {
 
         <div className="w-full border-b-2 border-[#767676] mt-[20px]"></div>
 
+        {/* 메뉴 리스트 */}
         <ul className="text-white text-[24px] tracking-wider font-semibold flex flex-col gap-[20px] mt-[40px] px-[10px]">
           <li>Community</li>
           <li>My Page</li>
@@ -92,24 +95,120 @@ export default function MinglePage() {
 
         <div className="w-full border-b-2 border-[#767676] mt-[40px]"></div>
 
-        <p className="text-white text-[24px] tracking-wider font-semibold mt-[20px] px-[10px]">
+        <p
+          className="text-white text-[24px] tracking-wider font-semibold mt-[20px] px-[10px]"
+          onClick={() => setDrawerOpen((prev) => !prev)}
+        >
           Component
         </p>
 
+        {/* 검색 바 */}
         <input
           type="text"
           placeholder="Search"
           className="w-[200px] h-[40px] bg-[#222222] rounded-lg mt-[20px] pl-[40px] text-white bg-[url(/search.png)] bg-no-repeat bg-[center_left_10px] shrink-0"
         />
 
+        {/* 컴포넌트 리스트 */}
         <ul
           className="mt-[20px] w-full h-full max-h-[520px] overflow-scroll text-white flex flex-col gap-[20px] px-[10px]"
           style={{ scrollbarColor: "#ffffff transparent" }}
+          onClick={() => setDrawerOpen((prev) => !prev)}
         >
           {new Array(30).fill("test").map((el, idx) => (
             <li>{`${el} ${idx + 1}`}</li>
           ))}
         </ul>
+      </div>
+
+      {/* 드로워 메뉴 */}
+      <div
+        className={`w-[320px] h-full bg-[#fcfcfc] fixed transition-all duration-300 ease-in-out rounded-r-2xl z-10 p-[20px]
+          ${isDrawerOpen ? "left-0" : "-left-[320px]"}`}
+      >
+        <div className="flex justify-end" onClick={() => setDrawerOpen((prev) => !prev)}>
+          <X color="#222222" strokeWidth={"3px"} size={32} />
+        </div>
+
+        <p className="text-[24px] font-semibold p-[10px]">컴포넌트 이름</p>
+
+        <div className="w-[260px] h-[180px] rounded-md border border-[#e0e0e0] m-[10px]">
+          <img />
+        </div>
+
+        <p className="p-[10px] leading-normal font-normal text-pretty tracking-wide max-h-[140px]">
+          사용자에게 해당 컴포넌트의 간략한 예시를 이미지로 보여주고, 각 부분에 어떤 요소가 들어갈
+          것인지 표현합니다.
+        </p>
+        <p className="px-[10px] leading-normal font-normal text-pretty tracking-wide max-h-[80px] text-[14px] flex gap-[4px]">
+          <p className="font-semibold">*</p>
+          <p>
+            일반적으로 {""}
+            <span className="text-[#52C41A]">성공</span>(초록), {""}
+            <span className="text-[#1677FF]">정보</span>(파랑), {""}
+            <span className="text-[#FAAD14]">경고</span>(주황), {""}
+            <span className="text-[#FF4D4F]">위험</span>(빨강)으로 표현합니다.
+          </p>
+        </p>
+
+        <div className="w-full border-b-2 border-[#e0e0e0] mt-[20px]"></div>
+
+        {/* 컴포넌트 리스트 */}
+        <div className="w-full max-h-[520px] overflow-scroll">
+          <div className="px-[10px] mt-[40px] flex flex-col gap-[20px]">
+            <div className="flex gap-[20px]">
+              <img src="/MUI.svg" alt="mui" className="mt-[4px]" />
+              <p className="text-[24px] font-semibold">Material UI</p>
+            </div>
+
+            <div
+              className="w-full h-[80px] rounded-md bg-[#e0e0e0] flex flex-col justify-center items-center"
+              onClick={onClickMUI}
+            >
+              <MUI_ALERT />
+              무이 무이 구글 디자인은 정말로 위대헤
+            </div>
+          </div>
+
+          <div className="px-[10px] mt-[40px] flex flex-col gap-[20px]">
+            <div className="flex gap-[20px]">
+              <img src="/ANTD.svg" alt="antd" className="mt-[4px]" />
+              <p className="text-[24px] font-semibold">Ant Design</p>
+            </div>
+
+            <div
+              className="w-full h-[80px] rounded-md bg-[#e0e0e0] flex flex-col justify-center items-center"
+              onClick={onClickANTD}
+            >
+              <ANTD_ALERT />
+              많이는 쓰는데... 뭔가... 뭔가... 별로...
+            </div>
+          </div>
+
+          <div className="px-[10px] mt-[40px] flex flex-col gap-[20px]">
+            <div className="flex gap-[20px]">
+              <img src="/CHAKRA.svg" alt="chakra" className="mt-[4px]" />
+              <p className="text-[24px] font-semibold">chakra UI</p>
+            </div>
+
+            <div className="w-full h-[80px] rounded-md bg-[#e0e0e0] flex justify-center items-center">
+              진정한 힙스터를 위한 낭만의 굳이굳이
+            </div>
+          </div>
+
+          <div className="px-[10px] mt-[40px] flex flex-col gap-[20px]">
+            <div className="flex gap-[20px]">
+              <img src="/SHADCN.svg" alt="shadcn" className="mt-[4px]" />
+              <p className="text-[24px] font-semibold">shadcn/ui</p>
+            </div>
+
+            <div className="w-full h-[80px] rounded-md bg-[#e0e0e0] flex justify-center items-center">
+              사실 샤드는 ui 콜렉숀이래요
+            </div>
+          </div>
+
+          <div className="h-[220px]"></div>
+        </div>
       </div>
 
       {/* 플로우 메인 화면 */}
